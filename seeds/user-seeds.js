53b554a52b757e56dad89601e365e31a3d57ffb5
Ying Ying <yingying@KuangYesMacBook.attlocal.net>
2022-11-09 13:48:51 -0800

    add content in user seeds

---
+++ seeds/user-seeds.js
@@ -0,0 +1,29 @@
+const sequelize = require('../config/connection');
+const { User, Post } = require('../models');
+
+const userData = [
+    {
+        username: 'rebeccao',
+        email: 'rebeccao@mail.com',
+        password: 'password123'
+    },
+    {
+        username: 'alexisz',
+        email: 'alexisz@mail.com',
+        password: 'password123'
+    },
+    {
+        username: 'mouam',
+        email: 'mouam@mail.com',
+        password: 'password123'
+    },
+    {
+        username: 'yingl',
+        email: 'yingl@mail.com',
+        password: 'password123'
+    },
+];
+
+const seedUsers = () => User.bulkCreate(userData, { individualtHooks: true });
+module.exports = seedUsers;
+
