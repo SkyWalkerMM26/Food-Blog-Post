1410b88b76c64e4ced94a3b104caedc56ab49f83
SkyWalkerMM26 <myia_moua26@yahoo.com>
2022-11-09 13:41:12 -0800

    create server.js to have all the packages that is going to be used for this application

---
+++ server.js
@@ -0,0 +1,44 @@
+//Express is a back end web application framework for building RESTful APIs with Node.js
+const express = require('express');
+// require our controllers file that has the implementing codes.
+const routes = require('./controllers');
+//referring to protected mysql login info
+const sequelize = require('.config/connection');
+//The node:path module provides utilities for working with file and directory paths. It can be accessed using:
+const path = require('path');
+// protects env mysql login info.
+require('dotenv').config();
+// requiring handlebars which is a templating engines for website applications.
+const exphbs = require('express-handlebars');
+//keeps track of user
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUnitialized: false,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+app.use(session(sess));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
+
+
+
+
