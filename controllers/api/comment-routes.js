495e59f610daedaaf20ea8e97e5d74a9e16325d5
SkyWalkerMM26 <myia_moua26@yahoo.com>
2022-11-10 09:28:09 -0800

    delete request as a logged in user

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -25,3 +25,24 @@ router.post('/', withAuth, (req, res) => {
       });
   }
 });
+
+router.delete('/:id', withAuth, (req, res) => {
+    Comment.destroy({
+        where: {
+          id: req.params.id
+        }
+      })
+        .then(dbCommentData => {
+          if (!dbCommentData) {
+            res.status(404).json({ message: 'No comment found' });
+            return;
+          }
+          res.json(dbCommentData);
+        })
+        .catch(err => {
+          console.log(err);
+          res.status(500).json(err);
+        });
+    });
+
+module.exports = router;
