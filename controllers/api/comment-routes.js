15671e73167bb4325d4ced2f8fa95744881d7c45
SkyWalkerMM26 <myia_moua26@yahoo.com>
2022-11-10 09:22:57 -0800

    post request to make a comment on page and have to be logged in user

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -10,3 +10,18 @@ router.get('/', (req,res) => {
           res.status(500).json(err);
           });
 });
+
+router.post('/', withAuth, (req, res) => {
+  if (req.session) {
+    Comment.create({
+      comment_text: req.body.comment_text,
+      post_id: req.body.post_id,
+      user_id: req.session.user_id
+    })
+      .then(dbCommentData => res.json(dbCommentData))
+      .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  }
+});
