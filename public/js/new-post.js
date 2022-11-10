5f2b215b7a4caf80f25213be67f03a34687be919
Ying Ying <yingying@KuangYesMacBook.attlocal.net>
2022-11-10 14:13:01 -0800

    created response function to make sure new post will be response

--- public/js/new-post.js
+++ public/js/new-post.js
@@ -3,5 +3,16 @@ async function newPostHandler(event) {
     const title = document.querySelector('input[name="name-input"]').value;
     const text = document.querySelector('input[name="comment-input"]').value;
 
+    const response = await fetch('/api/post-routes', {
+        method: 'POST',
+        body: JSON.stringify({
+            title,
+            text
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+
     
 }
