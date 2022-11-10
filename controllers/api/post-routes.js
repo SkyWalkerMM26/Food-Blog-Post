d2fd9c3b6ba706e29009870b6c30055adfac14e8
SkyWalkerMM26 <myia_moua26@yahoo.com>
2022-11-10 11:40:04 -0800

    doing a team push

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const { User, Post, Comment } = require('../../models');
 const sequelize = require('../../config/connection');
-const withAuth = require("../../utils/auth");
+const withAuth = require('../../utils/auth');
 
 //get api/posts/ (all posts)
 router.get('/', (req,res) => {
@@ -40,5 +40,26 @@ router.get('/', (req,res) => {
         res.status(500).json(err);
     });
 });
+
+router.get('/:id', (req, res) => {
+    Post.findOne({
+        where: {
+            //id which is from the values parsed from the URL path.
+            id: req.params.id
+        },
+        attributes: ['id','title', 'content_box', 'references','user_id', 'created_at'],
+        include: {
+            model: User,
+            attributes: ['username']
+        }
+    })
+    .then(dbPostData => {
+        if (!dbPostData) {
+            res.status(404).json({ message: ' No post found with this id'});
+        }
+    })
+
+    })
+})
 
    
