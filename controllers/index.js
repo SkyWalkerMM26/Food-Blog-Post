f0d48b5da7faabb9625c8543121276c46122a982
SkyWalkerMM26 <myia_moua26@yahoo.com>
2022-11-13 10:54:04 -0800

    create home-routes.js controller for the home page.

---
+++ controllers/index.js
@@ -0,0 +1,14 @@
+const router = require('express').Router();
+const apiRoutes = require('./api');
+const homeRoutes = require('./home-routes.js');
+const blogPostRoutes = require('./blogPost-routes.js');
+
+router.use('/api', apiRoutes);
+router.use('/', homeRoutes);
+router.use('/blog-post', blogPostRoutes);
+
+router.use((req, res) => {
+  res.status(404).end();
+});
+
+module.exports = router;
