72e1f8b06b8a2e4e46303d084fd3ff9b0ea1c2f8
SkyWalkerMM26 <myia_moua26@yahoo.com>
2022-11-13 10:24:45 -0800

    route.post to /login to check if password input by user match password in db

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -12,4 +12,24 @@ router.post('/login', async (req, res) => {
         .json({ message: 'Incorrect email or password, please try again' });
       return;
     }
-    
+    //checking if password matches
+    const validPassword = await userData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password, please try again' });
+      return;
+    }
+
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+      
+      res.json({ user: userData, message: 'You are now logged in!' });
+    });
+
+    } catch (err) {
+    res.status(400).json(err);
+  }
+});
