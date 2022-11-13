4cd73d803ba832b6e7f2ec1a3514d6e74d22b2c8
SkyWalkerMM26 <myia_moua26@yahoo.com>
2022-11-13 10:14:07 -0800

    do a router put to allow users to update their post and only doable if they are logged in

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -76,4 +76,28 @@ router.get('/:id', (req, res) => {
       });
   });
 
+
+  //loggedIn to update post
+  router.put('/:id', withAuth, (req, res) => {
+    Post.update(req.body,
+        {
+            where: {
+                id: req.params.id
+            }
+        }
+    )
+    .then(dbPostData => {
+        if (!dbPostData) {
+            res.status(404).json({ message: 'No post found with this id' });
+            return;
+        }
+        res.json(dbPostData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err)
+    });
+});
+
+
    
