0998832c76a7f738b31a575d8488f286e1e1ed2f
SkyWalkerMM26 <myia_moua26@yahoo.com>
2022-11-13 10:25:43 -0800

    create a router.post to /logout

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -33,3 +33,15 @@ router.post('/login', async (req, res) => {
     res.status(400).json(err);
   }
 });
+
+router.post('/logout', (req, res) => {
+  if (req.session.logged_in) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
+
+module.exports = router;
