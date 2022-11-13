a56b14822fadc9bd97d9addb4b3489ed8791114f
SkyWalkerMM26 <myia_moua26@yahoo.com>
2022-11-13 10:55:53 -0800

    create blog post route and set the reference files as variable

---
+++ controllers/blogPostRoutes.js
@@ -0,0 +1,5 @@
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Post, User, Comment } = require('../models');
+const withAuth = require('../utils/auth')
+
