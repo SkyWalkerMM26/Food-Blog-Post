8cd7887d929abc5f722823f7922d77aff8ed4fb3
SkyWalkerMM26 <myia_moua26@yahoo.com>
2022-11-13 10:23:18 -0800

    router.post to compare email input to email in db when user is trying to login

---
+++ controllers/api/userRoutes.js
@@ -0,0 +1,15 @@
+const router = require('express').Router();
+const { User } = require('../../models');
+
+// matching login email to what is in database to login
+router.post('/login', async (req, res) => {
+  try {
+    const userData = await User.findOne({ where: { email: req.body.email } });
+
+    if (!userData) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password, please try again' });
+      return;
+    }
+    
