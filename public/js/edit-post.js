598181db2cd0102629a56a2ff5971a78e8316ba6
Ying Ying <yingying@KuangYesMacBook.attlocal.net>
2022-11-12 18:19:13 -0800

    set response route for when new post is edit

--- public/js/edit-post.js
+++ public/js/edit-post.js
@@ -4,4 +4,18 @@ async function editFormHandler(event) {
     const id = window.location.toString().split('/')[
         window.location.toString().split('/').length - 1
     ];
+
+    const title = document.querySelector('input[name="post-title"]').value;
+    const postText = document.querySelector('textarea[name="post-text"]').value;
+
+    const response = await fetch(`api/posts/${id}`, {
+        method: 'PUT',
+        body: JSON.stringify({
+            title,
+            postText
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
 }
