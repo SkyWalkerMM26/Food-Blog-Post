68290d30ba022f37459160a4e30dd4fe26405940
Rebecca Overton <rebeccaoverton@Rebeccas-MacBook-Air.local>
2022-11-13 21:59:41 -0800

    minor changes, troubleshoot attempt

--- public/js/login.js
+++ public/js/login.js
@@ -37,7 +37,7 @@ const signupFormHandler = async (event) => {
     });
 
     if (response.ok) {
-      document.location.replace("/profile");
+      document.location.replace("/login");
     } else {
       alert(response.statusText);
     }
