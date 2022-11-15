4ab64b43a5a29efcf1fd915831ed56fe9b937510
Rebecca Overton <rebeccaoverton@Rebeccas-MacBook-Air.local>
2022-11-15 04:03:53 -0800

    a TON of seeds added

--- seeds/comment-seeds.js
+++ seeds/comment-seeds.js
@@ -3,19 +3,79 @@ const { Comment } = require('../models');
 const commentData = [
     {
         content: 'Looks yummy',
-        user_id: 1,
+        user_id: 4,
         post_id: 1
     },
     {
         content: 'What a feast!',
         user_id: 2,
-        post_id: 2
+        post_id: 1
     },
     {
         content: 'Am starving',
+        user_id: 1,
+        post_id: 1
+    },
+    {
+        content: 'I just tried it for the first time! Can`t wait to try it again on my next trip to SF',
+        user_id: 4,
+        post_id: 2
+    },
+    {
+        content: 'I want to give Little Sweet a try!',
         user_id: 3,
         post_id: 3
-    }
+    },
+    {
+        content: 'OOOO that looks yummy!',
+        user_id: 2,
+        post_id: 4
+    },
+    {
+        content: 'Those look so filling!',
+        user_id: 1,
+        post_id: 5
+    },
+    {
+        content: 'That sounds incredible!',
+        user_id: 1,
+        post_id: 6
+    },
+    {
+        content: 'I love hanging out there!',
+        user_id: 1,
+        post_id: 7
+    },
+    {
+        content: 'Did you know that you can rent the tents out for private events? I had my birthday party there with a fire pit and s`mores.',
+        user_id: 2,
+        post_id: 7
+    },
+    {
+        content: 'The best!!!!',
+        user_id: 1,
+        post_id: 8
+    },
+    {
+        content: 'That view is breathtaking. Will definitely make trip to that bar.',
+        user_id: 4,
+        post_id: 9
+    },
+    {
+        content: 'WOW!',
+        user_id: 3,
+        post_id: 10
+    },
+    {
+        content: 'I love their poli papas.',
+        user_id: 2,
+        post_id: 11
+    },
+    {
+        content: 'Thanks for sharing! Will give it a try.',
+        user_id: 1,
+        post_id: 12
+    },
 ];
 
 const seedComments = () => Comment.bulkCreate(commentData);
--- seeds/post-seeds.js
+++ seeds/post-seeds.js
@@ -4,6 +4,7 @@ const postData = [
     {
         title: 'What Diet?',
         category: 'Korean',
+        date_created: '11/13/2022',
         content_box: 'Kogi Gogi is a must have if you ever visit the inner sunset district in San Francisco! It`s a bit pricey for all you can eat (around $38 dollars per person), but definitely worth it if you come with an empty stomach. The service is fast, and they are very generous with their portions. The corn cheese is a must!',
         image: 'https://i.postimg.cc/W4tsp5yX/kbbq.png',
         user_id: 3
@@ -11,77 +12,88 @@ const postData = [
     {
         title: 'Damn Delicious',
         category: 'Dessert',
-        content_box: 'This is res is offering amazing food',
+        date_created: '11/13/2022',
+        content_box: 'A San Francisco classic! The banana splits are delicious, but too big for just one person. I love trying new flavors every time I visit. Just be prepared for the long wait in the chilly weather.',
         image: 'https://i.postimg.cc/tg9X0XdQ/mitchells.jpg',
         user_id: 1
     },
     {
         title: 'Wow, Yum',
         category: 'Dessert',
-        content_box: 'The best breakfast in town',
+        date_created: '11/13/2022',
+        content_box: 'My favorite after dinner treat.',
         image: 'https://i.postimg.cc/9FTkD8pP/boba.jpg',
         user_id: 2
     },
     {
-        title: 'How to make a peanut butter bread',
+        title: 'For the adventurous foodies..',
         category: 'Indian',
-        content_box: 'Follow the ingredients and steps as below',
+        date_created: '11/13/2022',
+        content_box: 'Indian food is not for everyone. The spices might be overwhelming to some. I think that Noori is a good introduction to the cuisine for those that are not too familiar. The portions are fairly small, however this allows you to try many different things. The paneer tikka masala is a great vegetarian alternative and one of my favorites.',
         image: 'https://i.postimg.cc/1z6X2Zcn/indian.jpg',
-        user_id: 3
+        user_id: 4
     },
     {
-        title: 'How to make a peanut butter bread',
-        category: 'Indian',
-        content_box: 'Follow the ingredients and steps as below',
+        title: 'IYKYK',
+        category: 'Italian',
+        date_created: '11/13/2022',
+        content_box: 'BLT with avocado on dutch crunch, please!',
         image: 'https://i.postimg.cc/BnRc24nX/little.jpg',
         user_id: 3
     },
     {
-        title: 'How to make a peanut butter bread',
-        category: 'Indian',
-        content_box: 'Follow the ingredients and steps as below',
+        title: 'Speechless',
+        category: 'Italian',
+        date_created: '11/13/2022',
+        content_box: 'I recently visited Boston for the first time and made a trip to Little Italy in North End. I had heard a lot about a restaurant called Trattoria Il Panino and decided to give it a try. It did not disappoint! I had glass of wine paired with their lobster ravioli with crab meat sauce and it was delicious. One of my favorite meals that I have had in a long time. This restaurant will definitely be my go-to whenever I return to Boston.',
         image: 'https://i.postimg.cc/MT5Q3Sq0/pasta.jpg',
-        user_id: 3
+        user_id: 2
     },
     {
-        title: 'How to make a peanut butter bread',
+        title: 'The Spot',
         category: 'Other',
-        content_box: 'Follow the ingredients and steps as below',
+        date_created: '11/13/2022',
+        content_box: 'Spark Social SF is my favorite place to invite friends and hangout. It is a lot easier to enjoy during the summer when the weather is a bit warmer. Every day they feature new food trucks which is perfect for someone who loves to try new restaurants. I always have such a fun time eating and enjoying the outdoors in the heart of SF.',
         image: 'https://i.postimg.cc/dQ7CmggC/spark.png',
         user_id: 3
     },
     {
-        title: 'How to make a peanut butter bread',
+        title: 'A taste of central america',
         category: 'Other',
-        content_box: 'Follow the ingredients and steps as below',
+        date_created: '11/13/2022',
+        content_box: 'The best place for authentic pupusas in the Bay Area.',
         image: 'https://i.postimg.cc/X7WQHwd4/balompie.jpg',
-        user_id: 3
+        user_id: 4
     },
     {
-        title: 'How to make a peanut butter bread',
+        title: 'Drinks with a view',
         category: 'Other',
-        content_box: 'Follow the ingredients and steps as below',
+        date_created: '11/13/2022',
+        content_box: 'The perfect happy hour spot! Enjoy drinks and dessert with an incredible view of the SF skyline.',
         image: 'https://i.postimg.cc/2SKpNb83/view.jpg',
-        user_id: 3
+        user_id: 1
     },
     {
-        title: 'How to make a peanut butter bread',
-        category: 'Other',
-        content_box: 'Follow the ingredients and steps as below',
+        title: 'What dreams are made of',
+        category: 'American',
+        date_created: '11/13/2022',
+        content_box: 'This brioche french toast with marscapone filling is my guilty pleasure. It is very rich so it really is a great treat to have every once in a while.',
         image: 'https://i.postimg.cc/NFk7qzmY/kitchen.jpg',
-        user_id: 3
+        user_id: 2
     },
     {
-        title: 'How to make a peanut butter bread',
-        category: 'Other',
-        content_box: 'Follow the ingredients and steps as below',
+        title: 'MMMMMM',
+        category: 'Spanish',
+        date_created: '11/13/2022',
+        content_box: 'The BEST mid-afternoon snack. Limón`s ceviche trio is always a good idea. Don`t forget to add a side of their yuca fries!',
         image: 'https://i.postimg.cc/NMMwr3GC/limon.jpg',
-        user_id: 3
+        user_id: 4
     },
     {
-        title: 'How to make a peanut butter bread',
-        category: 'Other',
-        content_box: 'Follow the ingredients and steps as below',
+        title: 'Guilt-free and Tasty!',
+        category: 'Greek',
+        date_created: '11/13/2022',
+        content_box: 'Souvla is my favorite after-gym meal. It is light, and the ingredients are always fresh.',
         image: 'https://i.postimg.cc/0jydvCfw/souvla.jpg',
         user_id: 3
     },
